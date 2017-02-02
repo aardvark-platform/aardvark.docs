ee010a84ea37e84849afa0b6490b5f871d401f58
Stefan Maierhofer <sm@stefanmaierhofer.com>
2017-02-02 08:48:13 +0100

    little gravity simulation

--- src/AdaptiveGeometry/Program.fs
+++ src/AdaptiveGeometry/Program.fs
@@ -1,4 +1,5 @@
 ﻿open System
+open System.Diagnostics
 open Aardvark.Base
 open Aardvark.Base.Rendering
 open Aardvark.Base.Incremental
@@ -16,53 +17,70 @@ let main argv =
     // simple OpenGL window
     use app = new OpenGlApplication()
     let win = app.CreateSimpleRenderWindow()
-    win.Text <- "AdaptiveGeometry (aardvark.docs)"
+    win.Text <- "PointSprites (aardvark.docs)"
     
     // view, projection and default camera controllers
-    let initialView = CameraView.lookAt (V3d(16.0, 11.0, 6.0)) V3d.Zero V3d.OOI
+    let initialView = CameraView.lookAt (V3d(50.0, 50.0, 50.0)) V3d.Zero V3d.OOI
     let view = initialView |> DefaultCameraController.control win.Mouse win.Keyboard win.Time
     let proj = win.Sizes |> Mod.map (fun s -> Frustum.perspective 60.0 0.1 1000.0 (float s.X / float s.Y))
 
-    // generate adaptive mesh
-    let mesh =
+    // generate points
+    let points =
+
+        let n = 50
+
+        let r = Random()
+        let mutable ps = [| for i in 1..n do yield 25.0f * (V3f(r.NextDouble(), r.NextDouble(), r.NextDouble()) - V3f(0.5, 0.5, 0.5)) |]
+        let vs = [| for i in 1..n do yield V3f(r.NextDouble() - 0.5, r.NextDouble() - 0.5, r.NextDouble() - 0.5) |]
+        let cs = [| for i in 1..n do yield C4b(r.NextDouble(), r.NextDouble(), r.NextDouble()) |]
+        let ms = [| for i in 1..n do yield float32(r.NextDouble()) |]
+
         let drawCall = 
             DrawCallInfo(
-                FaceVertexCount = 2 * 8,
+                FaceVertexCount = ps.Length,
                 InstanceCount = 1
             )
 
-        let positions = Mod.init [| V3f(0,0,0); V3f(-1,-1,0); V3f(1,-1,0); V3f(1,1,0); V3f(-1,1,0) |]
-        let normals =   Mod.init [| V3f.OOI; V3f.OOI; V3f.OOI; V3f.OOI; V3f.OOI |]
-        let colors =    Mod.init [| C4b.White; C4b.Red; C4b.Green; C4b.Blue; C4b.Yellow |]
-        //let indices =   Mod.constant [| 0;1;2; 0;2;3; 0;3;4; 0;4;1 |]
-        let indices =   Mod.constant [| 1;2; 2;3; 3;4; 4;1; 0;1; 0;2; 0;3; 0;4; |]
+        let positions = Mod.init ps
 
         async {
-            let mutable z = 0.0
+            let g = 0.2f
+            let sw = Stopwatch()
+            sw.Start()
+            let mutable t = 0.0f
+
             while true do
-                do! Async.Sleep 10
-                z <- z + 0.001
-                transact (fun () ->
-                    Mod.change positions [| V3f(0.0,0.0,z); V3f(-1,-1,0); V3f(1,-1,0); V3f(1,1,0); V3f(-1,1,0) |]
+                //do! Async.Sleep 10
+                ps <- ps |> Array.mapi (fun i p ->
+                    let mutable f = V3f.Zero
+                    for j in 1..ps.Length-1 do
+                        if i <> j then
+                            let d = ps.[j] - ps.[i]
+                            f <- f + g * d * ms.[i] * ms.[j] / d.LengthSquared
+                    vs.[i] <- vs.[i] + f
+
+                    let dt = float32 sw.Elapsed.TotalSeconds - t
+                    t <- t + dt
+                    ps.[i] + vs.[i] * dt
                 )
+                transact (fun () -> Mod.change positions ps)
         }
         |> Async.Start
 
         drawCall
-            |> Sg.render IndexedGeometryMode.LineList 
+            |> Sg.render IndexedGeometryMode.PointList 
             |> Sg.vertexAttribute DefaultSemantic.Positions positions
-            |> Sg.vertexAttribute DefaultSemantic.Normals normals
-            |> Sg.vertexAttribute DefaultSemantic.Colors colors
-            |> Sg.index indices
+            |> Sg.vertexAttribute DefaultSemantic.Colors (Mod.constant cs)
 
     // define scene
     let sg =
-        mesh
+        points
             |> Sg.effect [
                 DefaultSurfaces.trafo |> toEffect
                 DefaultSurfaces.vertexColor |> toEffect
-                DefaultSurfaces.simpleLighting |> toEffect
+                DefaultSurfaces.pointSprite |> toEffect
                ]
+            |> Sg.uniform "PointSize" (Mod.constant 10.0)
             |> Sg.viewTrafo (view |> Mod.map CameraView.viewTrafo)
             |> Sg.projTrafo (proj |> Mod.map Frustum.projTrafo)
 
