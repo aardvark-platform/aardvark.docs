8d4723eebadddc0597357d613443762c0165ae54
Stefan Maierhofer <sm@stefanmaierhofer.com>
2017-09-05 13:14:27 +0200

    fixing/improving build system

--- build.fsx
+++ build.fsx
@@ -1,11 +1,13 @@
-#load @"paket-files/build/vrvis/Aardvark.Fake/DefaultSetup.fsx"
+#load @"paket-files/build/aardvark-platform/Aardvark.Fake/DefaultSetup.fsx"
 
 open Fake
 open System
 open System.IO
 open System.Diagnostics
 open Aardvark.Fake
+open Fake.Testing
 
+do MSBuildDefaults <- { MSBuildDefaults with Verbosity = Some Minimal }
 do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
 DefaultSetup.install ["src/aardvark.docs.sln"]
