4b5f414fa92280fbb42ae5fcc42d3b9d7ce3888f
Thomas Ortner <ortner@vrvis.at>
2017-10-02 16:00:59 +0200

    added lense demo

---
+++ src/ModelUpdateView/Lenses.fs
@@ -0,0 +1,20 @@
+﻿namespace Lenses
+
+
+
+open Aardvark.Base             // for math such as V3d
+open Aardvark.Base.Incremental // for Mods etc and [<DomainType>]
+open Aardvark.Base.Rendering   // for render attribs such as cullMode
+open Aardvark.UI.Primitives    // for primitives such as camera controller state
+
+open LensesModel
+
+module LensesStuff =
+
+    let updateC (m : A) =
+        { m with b = { m.b with c = { m.b.c with value = "helo world" }}}
+
+
+
+
+
---
+++ src/ModelUpdateView/LensesModel.g.fs
@@ -0,0 +1,118 @@
+namespace LensesModel
+
+open System
+open Aardvark.Base
+open Aardvark.Base.Incremental
+open LensesModel
+
+[<AutoOpen>]
+module Mutable =
+
+    
+    
+    type MC(__initial : LensesModel.C) =
+        inherit obj()
+        let mutable __current = __initial
+        let _value = ResetMod.Create(__initial.value)
+        
+        member x.value = _value :> IMod<_>
+        
+        member x.Update(v : LensesModel.C) =
+            if not (System.Object.ReferenceEquals(__current, v)) then
+                __current <- v
+                
+                ResetMod.Update(_value,v.value)
+                
+        
+        static member Create(__initial : LensesModel.C) : MC = MC(__initial)
+        static member Update(m : MC, v : LensesModel.C) = m.Update(v)
+        
+        override x.ToString() = __current.ToString()
+        member x.AsString = sprintf "%A" __current
+        interface IUpdatable<LensesModel.C> with
+            member x.Update v = x.Update v
+    
+    
+    
+    [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+    module C =
+        [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+        module Lens =
+            let value =
+                { new Lens<LensesModel.C, Microsoft.FSharp.Core.string>() with
+                    override x.Get(r) = r.value
+                    override x.Set(r,v) = { r with value = v }
+                    override x.Update(r,f) = { r with value = f r.value }
+                }
+    
+    
+    type MB(__initial : LensesModel.B) =
+        inherit obj()
+        let mutable __current = __initial
+        let _c = MC.Create(__initial.c)
+        
+        member x.c = _c
+        
+        member x.Update(v : LensesModel.B) =
+            if not (System.Object.ReferenceEquals(__current, v)) then
+                __current <- v
+                
+                MC.Update(_c, v.c)
+                
+        
+        static member Create(__initial : LensesModel.B) : MB = MB(__initial)
+        static member Update(m : MB, v : LensesModel.B) = m.Update(v)
+        
+        override x.ToString() = __current.ToString()
+        member x.AsString = sprintf "%A" __current
+        interface IUpdatable<LensesModel.B> with
+            member x.Update v = x.Update v
+    
+    
+    
+    [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+    module B =
+        [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+        module Lens =
+            let c =
+                { new Lens<LensesModel.B, LensesModel.C>() with
+                    override x.Get(r) = r.c
+                    override x.Set(r,v) = { r with c = v }
+                    override x.Update(r,f) = { r with c = f r.c }
+                }
+    
+    
+    type MA(__initial : LensesModel.A) =
+        inherit obj()
+        let mutable __current = __initial
+        let _b = MB.Create(__initial.b)
+        
+        member x.b = _b
+        
+        member x.Update(v : LensesModel.A) =
+            if not (System.Object.ReferenceEquals(__current, v)) then
+                __current <- v
+                
+                MB.Update(_b, v.b)
+                
+        
+        static member Create(__initial : LensesModel.A) : MA = MA(__initial)
+        static member Update(m : MA, v : LensesModel.A) = m.Update(v)
+        
+        override x.ToString() = __current.ToString()
+        member x.AsString = sprintf "%A" __current
+        interface IUpdatable<LensesModel.A> with
+            member x.Update v = x.Update v
+    
+    
+    
+    [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+    module A =
+        [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+        module Lens =
+            let b =
+                { new Lens<LensesModel.A, LensesModel.B>() with
+                    override x.Get(r) = r.b
+                    override x.Set(r,v) = { r with b = v }
+                    override x.Update(r,f) = { r with b = f r.b }
+                }
