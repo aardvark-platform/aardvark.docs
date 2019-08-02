95fd0e3622e8087804846635f3ef4536e540716f
Evgeniy Andreev (gsomix) <gsomix@gmail.com>
2019-08-02 23:18:24 +0400

    Get rid of FAKE and Paket for build

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -f .paket/paket ]; then
-    dotnet tool install Paket --tool-path .paket
-fi
-
-./.paket/paket restore 
-
-dotnet packages/build/fake-cli/tools/netcoreapp2.1/any/fake-cli.dll build $@
